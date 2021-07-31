3d00499de8fb579d7568dca83b4816b1661432b2
DarkMatterMatt <mattmoran2000@gmail.com>
2021-08-01 09:32:39 +1200

    Add timer to youtubeView

--- components/youtubeView/index.js
+++ components/youtubeView/index.js
@@ -1,35 +1,65 @@
-const progressBar = document.querySelector(".ytp-progress-bar");
-const barColour = document.querySelector(".ytp-swatch-background-color");
+const youtubeView_progressBar = document.querySelector(".ytp-progress-bar");
+const youtubeView_barColour = document.querySelector(".ytp-swatch-background-color");
 
-const timeToWatch = 10; // 10 seconds for demo
-let timeViewed = 0;
+const youtubeView_timeToWatch = 30; // 30 seconds for demo
+let youtubeView_timeViewed = 0;
 
-let timer;
+let youtubeView_timer;
+const $youtubeView_timeRemaining = document.createElement("span");
 
 async function youtubeView() {
-  if (progressBar == null) {
+  if (youtubeView_progressBar == null) {
     console.log("youtubeView skipping because it only runs on video pages");
     return;
   }
 
-  startingTime = progressBar.getAttribute("aria-valuenow");
+  startingTime = youtubeView_progressBar.getAttribute("aria-valuenow");
 
-  timer = window.setInterval(updateTimeElapsed, 1000);
+  youtubeView_timer = window.setInterval(youtubeView_updateTimeElapsed, 1000);
+
+  youtubeView_addDOMElement();
 }
 
-async function updateTimeElapsed() {
+async function youtubeView_updateTimeElapsed() {
   const barColourValue = window
-    .getComputedStyle(barColour, null)
+    .getComputedStyle(youtubeView_barColour, null)
     .getPropertyValue("background-color");
 
   if (barColourValue === "rgb(255, 0, 0)") {
-    timeViewed++;
+    youtubeView_timeViewed++;
 
-    if (timeViewed >= timeToWatch) {
+    const remaining = Math.max(0, youtubeView_timeToWatch - youtubeView_timeViewed);
+    const mins = Math.floor(remaining / 60);
+    const secs = `${remaining % 60}`.padStart(2, "0");
+    $youtubeView_timeRemaining.innerText = `Time remaining: ${mins}:${secs}`;
+
+    if (remaining <= 0) {
       const balance = await addCoins(2000);
       showConfetti(2000, balance);
 
-      clearInterval(timer);
+      youtubeView_timeViewed = 0;
+      clearInterval(youtubeView_timer);
     }
   }
+}
+
+async function youtubeView_addDOMElement() {
+  const $elem = document.createElement("div");
+
+  $elem.style.position = "fixed";
+  $elem.style.zIndex = "999";
+  $elem.style.bottom = "0";
+  $elem.style.backgroundColor = "white";
+  $elem.style.width = "100%";
+  $elem.style.display = "flex";
+  $elem.style.padding = "10px 36px";
+  $elem.style.fontSize = "16px";
+
+  const $p = document.createElement("span");
+  $p.style.marginRight = "20px";
+  $p.innerText = `Watch 20 minutes to win ${coinsAvailableToWin} coins!`
+
+  $elem.appendChild($p);
+  $elem.appendChild($youtubeView_timeRemaining);
+  document.body.appendChild($elem);
 }
