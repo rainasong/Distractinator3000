333bd8b04ab630ff6d22183cf8238faac7272bb6
rainasong <rainasong0509@gmail.com>
2021-08-01 09:52:05 +1200

    Cleaned up popup.html & updated home

--- popup.js
+++ popup.js
@@ -1,28 +1,3 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("toggle");
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: loadDinoGame,
-  });
-});
-
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
-
 // toggle button
 let toggle = document.getElementById("toggle");
 
@@ -39,22 +14,7 @@ function toggleOption(){
   alert("hi");
 }
 
-let card = document.getElementById("card");
-
-// card.addEventListener("click", async () => {
-//   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-//   chrome.scripting.executeScript({
-//     target: { tabId: tab.id },
-//     function: getId,
-//   });
-// });
-
 document.addEventListener('click', function(e) {
   console.log(e.target.id);
   // alert(e.target.id);
 }, false);
-
-document.write('<comp></comp>');
-
-document.getElementById("test").innerHTML = "<comp></comp>";
