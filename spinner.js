b53e924f3357c10a4c7de2c75124f3c41af0528a
rainasong <rainasong0509@gmail.com>
2021-08-01 15:41:34 +1200

    Updated spinning wheel layout + text orientation & coins + confetti

--- spinner.js
+++ spinner.js
@@ -5,16 +5,17 @@ spinBtn.addEventListener("click", function() {
     var max = 9999;
     var deg = Math.floor(Math.random() * (min-max)) + min;
     document.getElementById("spinner").style.transform = "rotate("+deg+"deg)";
-    // console.log(Math.abs(Math.floor(((deg+22.5)%360)/45)));
+
     var index = Math.abs(Math.floor(((deg+22.5)%360)/45))+1;
-    // console.log(document.getElementById("index").innerHTML);
     if (index == 9){
         index = 1;
     }
+
     var coins = Number.parseInt(document.getElementById(index).innerText);
-    addCoins(coins);
+    
     setTimeout(function(){
         showConfetti(coins, getCoins());
+        addCoins(coins);
     }, 5500);
 });
 
