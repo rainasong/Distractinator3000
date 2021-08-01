25b95305aa6cc817c156709b07512486e5bac157
beverleysun <beverleysun0205@gmail.com>
2021-08-01 12:45:56 +1200

    Enable restarting of timer

--- components/facebookScroll/index.js
+++ components/facebookScroll/index.js
@@ -1,11 +1,25 @@
-let facebookMinutesRemaining = 20;
-let facebookSecondsRemaining = 0;
-let facebookTimeRemaining;
+const FACEBOOK_DEFAULT_MINUTES = 20;
+const FACEBOOK_DEFAULT_SECONDS = 0;
+
+let facebookMinutesRemaining;
+let facebookSecondsRemaining;
 let facebookCompleted = false;
+
 const facebookCoinsAvailableToWin = 1000;
+const $facebookTimeRemaining = document.createElement("p");
+const restartButton = document.createElement("button");
 
 async function facebookScroll() {
-  facebookTimeRemaining = document.createElement("p");
+  facebookMinutesRemaining = await getState(
+    "facebookScroll",
+    "facebookMinutesRemaining",
+    FACEBOOK_DEFAULT_MINUTES
+  );
+  facebookSecondsRemaining = await getState(
+    "facebookScroll",
+    "facebookSecondsRemaining",
+    FACEBOOK_DEFAULT_SECONDS
+  );
   facebookAddDOMElement();
   facebookStartCountdown();
 }
@@ -13,35 +27,75 @@ async function facebookScroll() {
 async function facebookStartCountdown() {
   setInterval(() => {
     if (document.visibilityState === "visible") {
-      if (facebookSecondsRemaining === 0 && facebookMinutesRemaining !== 0) {
-        facebookSecondsRemaining = 59;
-        facebookMinutesRemaining--;
-      } else if (
-        facebookSecondsRemaining >= 1 &&
-        facebookMinutesRemaining >= 0
-      ) {
-        facebookSecondsRemaining--;
-      }
-      facebookTimeRemaining.innerText = `Time remaining: ${facebookMinutesRemaining}:${facebookSecondsRemaining.toLocaleString(
+      facebookCountdown();
+      $facebookTimeRemaining.innerText = `Time remaining: ${facebookMinutesRemaining.toLocaleString(
         "en-US",
         {
           minimumIntegerDigits: 2,
           useGrouping: false,
         }
-      )}`;
+      )}:${facebookSecondsRemaining.toLocaleString("en-US", {
+        minimumIntegerDigits: 2,
+        useGrouping: false,
+      })}`;
     }
-
-    if (
-      !facebookCompleted &&
-      facebookMinutesRemaining <= 0 &&
-      facebookSecondsRemaining <= 0
-    ) {
-      addCoins(facebookCoinsAvailableToWin).then((res) => {
-        showConfetti(facebookCoinsAvailableToWin, res);
-      });
-      facebookCompleted = true;
-    }
+    facebookCheckCompleted();
   }, 1000);
+}
+
+function facebookCountdown() {
+  if (facebookSecondsRemaining === 0 && facebookMinutesRemaining !== 0) {
+    facebookSecondsRemaining = 59;
+    facebookMinutesRemaining--;
+    setState(
+      "facebookScroll",
+      "facebookSecondsRemaining",
+      facebookSecondsRemaining.toString()
+    );
+    setState(
+      "facebookScroll",
+      "facebookMinutesRemaining",
+      facebookMinutesRemaining.toString()
+    );
+  } else if (facebookSecondsRemaining >= 1 && facebookMinutesRemaining >= 0) {
+    facebookSecondsRemaining--;
+    setState(
+      "facebookScroll",
+      "facebookSecondsRemaining",
+      facebookSecondsRemaining.toString()
+    );
+  }
+}
+
+function facebookCheckCompleted() {
+  if (
+    !facebookCompleted &&
+    facebookMinutesRemaining <= 0 &&
+    facebookSecondsRemaining <= 0
+  ) {
+    addCoins(facebookCoinsAvailableToWin).then((res) => {
+      showConfetti(facebookCoinsAvailableToWin, res);
+    });
+    facebookCompleted = true;
+    restartButton.style.display = "inline-block";
+  }
+}
+
+async function resetTimer() {
+  setState(
+    "facebookScroll",
+    "facebookMinutesRemaining",
+    FACEBOOK_DEFAULT_MINUTES
+  );
+  setState(
+    "facebookScroll",
+    "facebookSecondsRemaining",
+    FACEBOOK_DEFAULT_SECONDS
+  );
+  facebookMinutesRemaining = FACEBOOK_DEFAULT_MINUTES;
+  facebookSecondsRemaining = FACEBOOK_DEFAULT_SECONDS;
+  facebookCompleted = false;
+  restartButton.style.display = "none";
 }
 
 async function facebookAddDOMElement() {
@@ -57,8 +111,25 @@ async function facebookAddDOMElement() {
   elem.style.padding = "10px 20px";
   elem.style.boxShadow = "0 3px 15px 2px rgba(0, 0, 0, 0.3)";
   elem.style.fontFamily = "Roboto";
-  elem.innerHTML = `<p style="margin-right:20px;">Scroll Facebook for 20 minutes to win ${facebookCoinsAvailableToWin} coins!</p>`;
-  elem.appendChild(facebookTimeRemaining);
+  elem.style.display = "flex";
+  elem.style.gap = "20px";
+  elem.style.alignItems = "center";
+  elem.innerHTML = `<p>Scroll Facebook for 20 minutes to win ${facebookCoinsAvailableToWin} coins!</p>`;
+
+  restartButton.innerText = "Again!";
+  restartButton.style.all = "unset";
+  restartButton.style.padding = "0 20px";
+  restartButton.style.height = "30px";
+  restartButton.style.borderRadius = "4px";
+  restartButton.style.color = "white";
+  restartButton.style.backgroundColor = "rgb(0, 119, 204)";
+  restartButton.style.margin = "0";
+  restartButton.style.cursor = "pointer";
+  restartButton.style.display = "none";
+  restartButton.addEventListener("click", resetTimer);
+
+  elem.appendChild($facebookTimeRemaining);
+  elem.appendChild(restartButton);
 
   html.appendChild(elem);
 }
